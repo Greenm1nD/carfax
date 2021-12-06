d3c56e86fdf4a43244a0eae2aef8653d829225a6
Greenm1nD <88585668+Greenm1nD@users.noreply.github.com>
2021-12-06 15:46:28 +0400

    responsive

--- assets/js/js.js
+++ assets/js/js.js
@@ -19,3 +19,23 @@ $('a[data-slide]').click(function (e) {
     var slideno = $(this).data('slide');
     $('.slider-nav').slick('slickGoTo', slideno - 1);
 });
+
+
+$(document).click(function() {
+    if(jQuery('.lang-select').find('.flag-img').hasClass('lang-show')){
+         jQuery('.lang-select').find('.flag-img').toggleClass('lang-show');		 
+      }
+ });
+ jQuery('.lang-selected').click(function(e){
+     e.preventDefault();
+   e.stopPropagation();
+     jQuery('.lang-select').find('.flag-img').toggleClass('lang-show');
+         
+ })
+ 
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+ 
+
+
+ 
