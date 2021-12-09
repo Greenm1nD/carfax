6ab037d85de502603fca03ef2eee5a45fffc09be
Greenm1nD <88585668+Greenm1nD@users.noreply.github.com>
2021-12-10 00:31:05 +0400

    add dark theme

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,18 +1,33 @@
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-});
+// $('.slider-for').slick({
+//     slidesToShow: reportWindowSize(),
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.slider-nav'
+// });
 
-$('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    focusOnSelect: true
-});
+// function reportWindowSize() {
+
+if (window.innerWidth <= 1024) {
+    $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        focusOnSelect: true
+    });
+} else if (window.innerWidth >= 1024) {
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        focusOnSelect: true
+    });
+}
+
+//   reportWindowSize()
+//   window.addEventListener('resize', reportWindowSize);
 
 $('a[data-slide]').click(function (e) {
     e.preventDefault();
@@ -21,21 +36,64 @@ $('a[data-slide]').click(function (e) {
 });
 
 
-$(document).click(function() {
-    if(jQuery('.lang-select').find('.flag-img').hasClass('lang-show')){
-         jQuery('.lang-select').find('.flag-img').toggleClass('lang-show');		 
-      }
- });
- jQuery('.lang-selected').click(function(e){
-     e.preventDefault();
-   e.stopPropagation();
-     jQuery('.lang-select').find('.flag-img').toggleClass('lang-show');
-         
- })
- 
+$(document).click(function () {
+    if (jQuery('.lang-select').find('.flag-img').hasClass('lang-show')) {
+        jQuery('.lang-select').find('.flag-img').toggleClass('lang-show');
+    }
+});
+jQuery('.lang-selected').click(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    jQuery('.lang-select').find('.flag-img').toggleClass('lang-show');
+
+})
+
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
- 
 
 
- 
+
+navToggle.addEventListener('click', () => {
+    let visibility = primaryNav.getAttribute('data-visible');
+    console.log(visibility);
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', false);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', true);
+    }
+})
+
+// dark theme
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
