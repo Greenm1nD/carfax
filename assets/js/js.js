92d284b093493ffbe193bbd5aeeab82cfdd81b55
Greenm1nD <88585668+Greenm1nD@users.noreply.github.com>
2021-12-15 12:30:57 +0400

    last change 6

--- assets/js/js.js
+++ assets/js/js.js
@@ -22,6 +22,7 @@ $('a[data-slide]').click(function (e) {
     $('.slider-nav').slick('slickGoTo', slideno - 1);
 });
 
+
 $(document).click(function () {
     if (jQuery('.lang-select').find('.flag-img').hasClass('lang-show')) {
         jQuery('.lang-select').find('.flag-img').toggleClass('lang-show');
@@ -31,6 +32,7 @@ jQuery('.lang-selected').click(function (e) {
     e.preventDefault();
     e.stopPropagation();
     jQuery('.lang-select').find('.flag-img').toggleClass('lang-show');
+
 })
 
 const primaryNav = document.querySelector('.primary-navigation');
@@ -38,6 +40,8 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 
 navToggle.addEventListener('click', () => {
     let visibility = primaryNav.getAttribute('data-visible');
+    console.log(visibility);
+
     if (visibility === "false") {
         primaryNav.setAttribute('data-visible', true);
         navToggle.setAttribute('aria-expanded', false);
@@ -48,50 +52,57 @@ navToggle.addEventListener('click', () => {
 })
 
 // dark theme
+
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
+
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
+
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    logoSwap();
-    // Add or remove the dark / icon theme
+    let getCurrentLogo = getCurrentTheme()==='dark' ? '/new/img/logo-2.png' : '/new/img/logo.png'
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
+    localStorage.setItem('logo', getCurrentLogo)
+    let logo = document.getElementById("logo-one");
+    logo.setAttribute("src",getCurrentLogo)
 })
 
-// outside click
-$(window).click(function () { 
+$(window).click(function() {
     primaryNav.setAttribute('data-visible', false);
     navToggle.setAttribute('aria-expanded', true);
     document.getElementById("arrow").checked = false;
 });
-
-$('#header').click(function (event) {
+  
+$('#header').click(function(event){
     event.stopPropagation();
 });
 
+logoSwap()
 function logoSwap() {
     let logo = document.getElementById("logo-one");
-    let src = logo.getAttribute("src")
-    if (src === "assets/img/logo.png") {
-        logo.setAttribute("src", "assets/img/logo-2.png")
-    } else {
-        logo.setAttribute("src", "assets/img/logo.png")
+    let localLogo = localStorage.getItem('logo')
+    if(!localLogo){ 
+        let src = "/new/img/logo-2.png" 
+        logo.setAttribute("src",src)
+        localStorage.setItem('logo',logo.getAttribute('src'))
+    }else{ 
+        logo.setAttribute("src",localLogo)
     }
 }
